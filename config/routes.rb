971b036b1d8d13073f85ca23e6195d315bd14231
od3n <mr.od3n@gmail.com>
2011-07-02 00:12:26 +0800

    Removed default Rails page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 FirstRailsProject::Application.routes.draw do
+  
+  
+  root :to => 'pages#home'
+  match '/contact', :to => 'pages#contact'
+  match '/about', :to => 'pages#about'
+  match '/help',  :to => 'pages#help'
+  
   get "pages/home"
   
   get "pages/about"
